adae9347cacda5b21f5b74c66f8cdb6c4210d671
Serhii Malyshev <malyshev.php@gmail.com>
2025-07-13 11:05:57 +0300

    doc: update README

--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,6 @@
 export const log = {
     error: (msg: string): void => console.error(`\x1b[31m🛑 ${msg}\x1b[0m`),
-    warn: (msg: string): void => console.warn(`\x1b[33m⚠️ ${msg}\x1b[0m`),
-    notice: (msg: string): void => console.info(`\x1b[36m📘 ${msg}\x1b[0m`),
-    success: (msg: string): void => console.log(`\x1b[32m✅ ${msg}\x1b[0m`),
+    warn: (msg: string): void => console.warn(`\x1b[33m🟡 ${msg}\x1b[0m`),
+    notice: (msg: string): void => console.info(`\x1b[36m🔵 ${msg}\x1b[0m`),
+    success: (msg: string): void => console.log(`\x1b[32m🟢 ${msg}\x1b[0m`),
 };
